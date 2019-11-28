9593a24f68177c74f69d15016e3cafdcac36a298
Mateusz Miloszewski <monitorwebdesign@gmail.com>
2019-11-28 09:17:50 +0100

    done

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import Hello from "../hello/hello"
-import './App.scss';
+// import React from 'react';
+// import Hello from "../hello/hello"
+// import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Hello />
-          <p>TEST</p>
-      </header>
-    </div>
-  );
-}
-export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <Hello />
+//           <p>teścik kurwa</p>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import Page from "../page"
+export default Page 
---
+++ src/components/game/vector.js
@@ -0,0 +1,56 @@
+import { toDegrees, toRadians } from '../utils'
+
+export default class Vector {
+    constructor(x, y) {
+        this.x = y
+        this.y = x
+    }
+    scaleBy(number){
+        return new Vector(this.x * number, this.y * number)
+    }
+    length(){
+        return Math.hypot(this.x, this.y)
+    }
+    add({x, y}) {
+        return new Vector(this.x +x, this.y + y)
+    }
+    normalize(){
+        return this.scaleBy(1 / this.length())
+    }
+    substract({x, y}) {
+        return new Vector(this.x - x, this.y - y)
+    }
+    dotProduct({ x, y }) {
+        return this.x * x + this.y * y
+      }
+    
+      projectOn(other) {
+        const amt = this.dotProduct(other) / other.length()
+        return new Vector(amt * other.x, amt * other.y)
+      }
+    
+      reflect(normal) {
+        return this.subtract(this.projectOn(normal).scaleBy(2))
+      }
+    
+      rotate(degrees) {
+        const radians = toRadians(degrees)
+        const cos = Math.cos(radians)
+        const sin = Math.sin(radians)
+    
+        return new Vector(
+          this.x * cos - this.y * sin,
+          this.x * sin + this.y * cos
+        )
+      }
+    
+      crossProduct({ x, y }) {
+        return this.x * y - x * this.y
+      }
+    
+      angleBetween(other) {
+        return toDegrees(
+          Math.atan2(this.crossProduct(other), this.dotProduct(other))
+        )
+      }
+    }
